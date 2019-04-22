206462c3b2046a7a5a2e336539973a5f0b60d9a6
Cyber <1534045943@qq.com>
2019-04-22 08:37:56 +0800

    添加子模块

---
+++ mini-mall-all/src/main/java/com/athos/mall/StarterApplication.java
@@ -0,0 +1,19 @@
+package com.athos.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.athos.mall.*.mapper")
+public class StarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
+}
